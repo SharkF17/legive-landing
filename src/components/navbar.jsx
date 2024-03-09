d440638c69fd044ab35419c7d6b59c17e8c3173c
Diego Miguel <eduardo.miguelg99@gmail.com>
2024-03-09 00:56:10 -0600

    terminando la funcionalidad del boton del navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,10 +1,28 @@
-export default function Navbar() {
+let menu;
+
+function Opn() {
+  menu = document.getElementById("menu");
+  menu.classList.add("w-[100%]");
+}
+
+function Clse() {
+  menu = document.getElementById("menu");
+  menu.classList.remove("w-[100%]");
+}
+
+function Navbar() {
   return (
     <>
-      <div className="fixed top-5 right-5 text-4xl text-legivegold cursor-pointer">
+      <div
+        className="fixed top-5 right-5 text-4xl text-legivegold cursor-pointer"
+        onClick={() => Opn()}
+      >
         <ion-icon name="menu-outline"></ion-icon>
       </div>
-      <nav className=" grid fixed w-[100%] h-[100vh] place-content-center">
+      <nav
+        className="bg-legiveblack grid fixed w-[0] h-[100vh] place-content-center overflow-hidden duration-150"
+        id="menu"
+      >
         <ul className="flex flex-col justify-center gap-y-10 text-white text-3xl">
           <li className="hover:translate-y-[-5px] duration-500">
             <a href="#" className="hover:textShadow">
@@ -32,7 +50,15 @@ export default function Navbar() {
             </a>
           </li>
         </ul>
+        <div
+          className="absolute top-5 right-5 text-4xl text-legivegold cursor-pointer"
+          onClick={() => Clse()}
+        >
+          <ion-icon name="close-outline"></ion-icon>
+        </div>
       </nav>
     </>
   );
 }
+
+export default Navbar;
